351b42ca2580cbe3dd7edb073809955522f13059
Jamie Atkinson <jabbslad@gmail.com>
2012-01-24 18:29:54 +0000

    added more attributes

--- scraper.js
+++ scraper.js
@@ -26,6 +26,17 @@ exports.job = new nodeio.Job(options, {
         this.getHtml('http://live.xbox.com/en-US/Profile?gamertag=' + encodeURIComponent(gamertag), function (err, $) {
             var gamerpic = $('img.gamerpic').attribs.src;
             var status = $('.presence').text;
+			var name = $('.name .value').text;
+			var location = $('.location .value').text;
+			var bio = $('.bio .value').text;
+            var motto = '';
+            try {
+                motto = $('.motto').text;
+            } catch (err) {/* motto class doesn't always exist */};
+            var rep = 0;
+            $('.reputation div').each(function(div) {
+                rep += repCalc(div);
+            });
             var gamerscore = $('.gamerscore').text;
             var online = false;
             switch(true) {
@@ -37,6 +48,11 @@ exports.job = new nodeio.Job(options, {
             		break;
             }
             client.hset("friends:" + gamertag, "gamerpic", gamerpic, redis.print);
+			client.hset("friends:" + gamertag, "name", name, redis.print);
+			client.hset("friends:" + gamertag, "location", location, redis.print);
+            client.hset("friends:" + gamertag, "rep", rep, redis.print);
+			client.hset("friends:" + gamertag, "bio", bio, redis.print);
+            client.hset("friends:" + gamertag, "motto", motto, redis.print);
             client.hset("friends:" + gamertag, "online", online, redis.print);
             client.hset("friends:" + gamertag, "status", status, redis.print);
             client.hset("friends:" + gamertag, "gamerscore", gamerscore, redis.print);
@@ -44,3 +60,23 @@ exports.job = new nodeio.Job(options, {
         });
     }
 });
+
+function repCalc(div) {
+    var cls = div.attribs.class;
+    var val = 0;
+    switch(true) {
+        case /Star Quarter/.test(cls):
+            val += 0.25;
+            break;
+        case /Star Half/.test(cls):
+            val += 0.50;
+            break;
+        case /Star ThreeQuarter/.test(cls):
+            val += 0.75;
+            break;
+        case /Star Full/.test(cls):
+            val += 1;
+            break;
+    }
+    return val;
+}
